e8c575977323bace46a925d422976b1b0ed62501
Isaaclope <105275690+Isaaclope@users.noreply.github.com>
2022-06-07 11:46:33 +0200

    Practicas-Modulo2

---
+++ Practica2/Arraypractica2.js
@@ -0,0 +1,15 @@
+let banco1 = [hola1,hola2,hola3,hola4];
+let banco2 = [hola5,hola6,hola7,hola9];
+let banco3 = [hola10,hola11,hola12,hola13];
+
+for (let i=0; i <banco1.length; i++){
+    console.log(banco1[i]);
+}
+
+for (let i=0; i <banco2.length; i++){
+    console.log(banco2[i]);
+}
+
+for (let i=0; i <banco3.length; i++){
+    console.log(banco3[i]);
+}
---
+++ Practica2/Practica2Objeto.js
@@ -0,0 +1,18 @@
+const banco1 = { 
+    gestor: hola1,
+    cliente: hola2,
+    mensaje: hola3,
+    transferencia:hola4
+};
+const banco2 = {
+    Gestor: hola5,
+    Cliente: hola6,
+    mensaje: hola7,
+    transferencia: hola8
+};
+const banco3 = {
+    Gestor: hola9,
+    Cliente: hola10,
+    mensaje: hola11,
+    transeferencia: hola12
+}
